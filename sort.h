9a911c200a30f39850e4a554d591e19c9b6ecf6c
TobyMike-max <osinubi.michael@gmail.com>
2022-07-19 22:09:23 +0100

    [Modified]: Added prototype for selection_sort function

--- sort.h
+++ sort.h
@@ -14,11 +14,10 @@
 
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
-
 
 
 void bubble_sort(int *array, size_t size);
@@ -26,7 +25,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-
+void selection_sort(int *array, size_t size);
 
 
 #endif /** _SORT_H_ */
