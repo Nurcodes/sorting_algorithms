f41860a1d28d7e942ced89e405c6d80afb1c00e8
Abdifatah Nur <deenynur21@gmail.com>
2022-07-19 15:44:19 +0300

    Task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sort a dll linked list
+ *
+ * @list: list to sort
+ *
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j;
+
+	if (!list)
+		return;
+
+	i = *list;
+
+	while (i)
+	{
+		while (i->next && (i->n > i->next->n))
+		{
+			j = i->next;
+			i->next = j->next;
+			j->prev = i->prev;
+
+			if (i->prev)
+				i->prev->next = j;
+
+			if (j->next)
+				j->next->prev = i;
+
+			i->prev = j;
+			j->next = i;
+
+			if (j->prev)
+				i = j->prev;
+			else
+				*list = j;
+
+			print_list(*list);
+		}
+		i = i->next;
+	}
+}
--- sort.h
+++ sort.h
@@ -24,7 +24,8 @@ struct listint_s *next;
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+listint_t *create_listint(const int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 
 
