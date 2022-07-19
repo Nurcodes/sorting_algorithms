73b5dbd125ecd2d38006971ff157079d1ea332f3
TobyMike-max <osinubi.michael@gmail.com>
2022-07-19 22:13:00 +0100

    [Modified]: Added prototype for quick_sort function

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void print_list(const listint_t *list);
 listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
-#endif /** _SORT_H_ */
+#endif /* _SORT_H_ */
