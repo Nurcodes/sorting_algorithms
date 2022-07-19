68df6a73cf414468de8297e89e4d888794db248e
Abdifatah Nur <deenynur21@gmail.com>
2022-07-20 02:11:20 +0300

    Task 2 fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,7 +19,7 @@ void selection_sort(int *array, size_t size)
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[min] > array[j])
+			if (array[j] < array[min])
 			{
 				min = j;
 			}
@@ -29,7 +29,7 @@ void selection_sort(int *array, size_t size)
 			tmp = array[i];
 			array[i] = array[min];
 			array[min] = tmp;
+			print_array(array, size);
 		}
-		print_array(array, size);
 	}
 }
