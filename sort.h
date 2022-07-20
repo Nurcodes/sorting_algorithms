eda4b2bb29aec4c46a9da34ffdcc4912867185e4
Abdifatah Nur <deenynur21@gmail.com>
2022-07-20 04:06:15 +0300

    Task quick sort finished

--- sort.h
+++ sort.h
@@ -27,6 +27,8 @@ listint_t *create_listint(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int partition(int *array, int first, int last, size_t size);
+void qs(int *array, int first, int last, size_t size);
 
 
 #endif /* _SORT_H_ */
